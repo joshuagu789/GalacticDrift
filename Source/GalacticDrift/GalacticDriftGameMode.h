0020d18bc60f6ea4c563eabd500617d95e886327
joshuagu789 <joshuagu789@gmail.com>
2024-06-23 15:07:25 -0400

    Default 3rd person scene

---
+++ Source/GalacticDrift/GalacticDriftGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GalacticDriftGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGalacticDriftGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGalacticDriftGameMode();
+};
+
+
+
