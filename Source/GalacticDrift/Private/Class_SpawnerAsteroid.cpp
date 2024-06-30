17c9553092cf48613a9f3dc4d0aa4069a17ab7bb
joshuagu789 <joshuagu789@gmail.com>
2024-06-29 21:31:58 -0400

    spawner for asteroids
    created blueprint that spawns asteroids in ellipse around itself, note random scale does not appear to be working

---
+++ Source/GalacticDrift/Private/Class_SpawnerAsteroid.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Class_Spawner.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+AClass_Spawner::AClass_Spawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AClass_Spawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AClass_Spawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AClass_Spawner::SpawnActorsEllipse(int min, int max, float max_x, float max_y, float max_z, float min_scale, float max_scale)
+{
+	if(actorPtr){
+		int count = UKismetMathLibrary::RandomIntegerInRange(min,max);
+		for(int i = 0; i < count; i++){
+			FVector location = GetTransform().GetLocation();	// hopefully copies FVector not reference
+
+			location.X += UKismetMathLibrary::RandomFloatInRange(-max_x,max_x);
+			location.Y += UKismetMathLibrary::RandomFloatInRange(-max_y,max_y);
+			location.Z += UKismetMathLibrary::RandomFloatInRange(-max_z,max_z);
+
+			FRotator rotation{UKismetMathLibrary::RandomFloatInRange(0,360),UKismetMathLibrary::RandomFloatInRange(0,360),UKismetMathLibrary::RandomFloatInRange(0,360)};
+			
+			float scale = UKismetMathLibrary::RandomFloatInRange(min_scale,max_scale);
+			FVector scaleVector{scale,scale,scale}; 
+
+			FTransform blankTransform;
+			blankTransform.SetLocation(location);
+			blankTransform.SetRotation(rotation.Quaternion());
+			blankTransform.SetScale3D(scaleVector);
+			//plan to also have random rotation and scale
+
+       		FActorSpawnParameters spawnParams;
+
+			AActor* temp = GetWorld()->SpawnActor<AActor>(actorPtr, blankTransform, spawnParams);
+			actorList.Add(temp);
+		}
+	}
+    else{
+        UE_LOG(LogTemp, Warning, TEXT("Warning: Pointer for subclass of AActor is null for spawner, method cancelled"));
+    }
+}
---
+++ Source/GalacticDrift/Public/Class_Spawner.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Class_Spawner.generated.h"
+
+UCLASS()
+class GALACTICDRIFT_API AClass_Spawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AClass_Spawner();
+
+    UFUNCTION(BlueprintCallable, Category="Action")
+		/*
+			min and max for range of actors to spawn, max_x is furthest x distance from center, same pattern for max_y and max_z
+		*/
+        void SpawnActorsEllipse(int min, int max, float max_x, float max_y, float max_z, float min_scale, float max_scale);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		//Experimental data structure- reasoning is lots of deletion (possibly) and each actor is unique
+		TSet<AActor*> actorList;	
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TSubclassOf<AActor> actorPtr;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
