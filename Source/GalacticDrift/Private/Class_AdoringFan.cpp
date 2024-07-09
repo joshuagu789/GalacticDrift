e64cf01f60afb5a9bc37e23f7f1e9c769704a0c3
joshuagu789 <joshuagu789@gmail.com>
2024-07-09 14:35:31 -0400

    beginning to replace mercenary with blueprint that can do spawn animation
    unsure why current bp mercenary cannot do animation

---
+++ Source/GalacticDrift/Private/Class_AdoringFan.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Class_AdoringFan.h"
+
+// Sets default values
+AClass_AdoringFan::AClass_AdoringFan()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AClass_AdoringFan::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AClass_AdoringFan::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AClass_AdoringFan::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
